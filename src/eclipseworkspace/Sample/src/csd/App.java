2f5b1ffbfb7821a7980ed4c146d7af708be2d40e
Oğuz Karan <oguzkaran@csystem.org>
2022-09-04 12:34:16 +0300

    Update

--- src/eclipseworkspace/Sample/src/csd/App.java
+++ src/eclipseworkspace/Sample/src/csd/App.java
@@ -1,25 +1,14 @@
 /*----------------------------------------------------------------------------------------------------------------------
-	
+	 Değişken isimlendirmede boşluk (whitespace) karakterler geçersizdir   
 -----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String [] args)
 	{		
-		float a = 0.1F, b = 0.2F;
-		float c;
+		int öğrencisayısı = 100;
 		
-		c = a + b;
-		
-		System.out.printf("a = %.20f%n", a);
-		System.out.printf("b = %.20f%n", b);
-		System.out.printf("c = %.20f%n", c);
-		
-		
-		if (c == 0.3)
-			System.out.println("Eşit");
-		else		
-			System.out.println("Eşit değil");
+		System.out.println(öğrencisayısı);
 	}
 }
 
