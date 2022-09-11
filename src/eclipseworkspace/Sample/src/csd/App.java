076c5acfe2f9aa8a834a7865a309f0fb3f0f6f02
Oğuz Karan <oguzkaran@csystem.org>
2022-09-11 12:29:21 +0300

    Update

--- src/eclipseworkspace/Sample/src/csd/App.java
+++ src/eclipseworkspace/Sample/src/csd/App.java
@@ -1,39 +1,33 @@
 /*---------------------------------------------------------------------------------------------------------------------
-	Bir metodun geri dönüş değeri başka bir metodun geri dönüş değeri olarak verilebilir 
+	Metot çağrısında bir argümana ilişkin ifade önce hesaplanır, elde edilen değer argüman olarak metoda geçilir. Birden
+	fazla parametreye sahip bir metot çağrısında tüm argümanlara ilişkin ifadeler hesaplanır, elde edilen değerler ile
+	metot çağrılır
+	
+	Anahtar Notlar: Aşağıdaki örnekte *** ile belirtilen deyime ilişkin ifadede kullanılan "compact" çözüm gibi kodlar
+	okunabilirliği/algılanabilirliği etkilememelidir. Eğer böyle bir durum varsa ayrı ayrı hesaplanacak şekilde kodlar 
+	yazılmalıdır
 -----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String [] args)
 	{		
-		int result;
+		java.util.Scanner kb = new java.util.Scanner(System.in);
 		
-		result = Util.getResult();
-		System.out.println(result);		
-	}
-}
-
-class Util {
-	public static int getResult()
-	{
-		System.out.println("İşlemi yapılıyor!...");
+		System.out.print("İki sayı giriniz:");
+		int a = kb.nextInt();
+		int b = kb.nextInt();	
 		
-		return NumberUtil.sum() * 2;
+		System.out.println(NumberUtil.sum(a + 2, b - 8)); //***
 	}
 }
 
 class NumberUtil {
-	public static int sum()
+	public static int sum(int a, int b) //error
 	{
-		java.util.Scanner kb = new java.util.Scanner(System.in);
-		
-		System.out.print("Birinci sayıyı giriniz:");
-		int a = Integer.parseInt(kb.nextLine());
-		
-		System.out.print("İkinci sayıyı giriniz:");
-		int b = Integer.parseInt(kb.nextLine());		
-		
 		return a + b;
-	}
+	}	
+	
+	//...
 }
 
