b0ea361639999cab4f4fc5d52c9bccc90ca3b7e5
Oğuz Karan <oguzkaran@csystem.org>
2022-09-11 10:33:17 +0300

    Update

--- src/eclipseworkspace/Sample/src/csd/App.java
+++ src/eclipseworkspace/Sample/src/csd/App.java
@@ -1,16 +1,5 @@
-/*----------------------------------------------------------------------------------------------------------------------
-	 Bir metodun geri dönüş değeri bilgisi yerine bir tür ismi yazılırsa bu metodun "geri dönüş değeri" vardır denir.
-	 Tersine bir metodun geri dönüş değeri varsa geri dönüş değeri bilgisi yerine bir tür yazılır. Bir metodun geri dönüş
-	 değeri varsa, metot çağrısı bittikten sonra çağrıldığı noktaya bir değer ile geri döner. Bir metodun geri dönüş değeri
-	 yoksa geri dönüş bilgisi yerine void anahtar sözcüğü yazılır. Bir metodun geri dönüş değeri metot içerisinde return 
-	 deyimi ile oluşturulur. return deyiminin genel biçimi şu şekildedir:
-	 	 
-	 	return [ifade];
-	 
-	 return deyimi nasıl kullanılırsa kullanılsın metodu sonlandırır. Yani metot içerisinde akış return deyimine 
-	 geldiğinde metot sonlanır ve akış çağrılan noktaya geri döner. return deyimine ilişkin ifadenin değeri çağıran
-	 metoda aktarılır. Aşağıdaki örnekte sum metodunun geri dönüş değeri 2 ile çarpılmış ve sonuç result değişkenine
-	 atanmıştır.  	 
+/*---------------------------------------------------------------------------------------------------------------------
+	Bir metodun geri dönüş değeri başka bir metodun geri dönüş değeri olarak verilebilir 
 -----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
@@ -19,13 +8,21 @@ class App {
 	{		
 		int result;
 		
-		result = Util.sum() * 2;
+		result = Util.getResult();
 		System.out.println(result);		
 	}
-	
 }
 
 class Util {
+	public static int getResult()
+	{
+		System.out.println("İşlemi yapılıyor!...");
+		
+		return NumberUtil.sum() * 2;
+	}
+}
+
+class NumberUtil {
 	public static int sum()
 	{
 		java.util.Scanner kb = new java.util.Scanner(System.in);
@@ -34,13 +31,9 @@ class Util {
 		int a = Integer.parseInt(kb.nextLine());
 		
 		System.out.print("İkinci sayıyı giriniz:");
-		int b = Integer.parseInt(kb.nextLine());
+		int b = Integer.parseInt(kb.nextLine());		
 		
-		int result = a + b;
-		
-		return result;
+		return a + b;
 	}
 }
-
-
 
