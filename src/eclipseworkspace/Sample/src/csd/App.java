19fe06e5ff00f7a1a36d408d7dbd7c3a255e0d51
Oğuz Karan <oguzkaran@csystem.org>
2022-09-17 10:30:05 +0300

    Update

--- src/eclipseworkspace/Sample/src/csd/App.java
+++ src/eclipseworkspace/Sample/src/csd/App.java
@@ -1,18 +1,23 @@
 /*---------------------------------------------------------------------------------------------------------------------
-	Metot çağrısında bir argümana ilişkin ifade önce hesaplanır, elde edilen değer argüman olarak metoda geçilir. Birden
-	fazla parametreye sahip bir metot çağrısında tüm argümanlara ilişkin ifadeler hesaplanır, elde edilen değerler ile
-	metot çağrılır
-	
-	Anahtar Notlar: Aşağıdaki örnekte *** ile belirtilen deyime ilişkin ifadede kullanılan "compact" çözüm gibi kodlar
-	okunabilirliği/algılanabilirliği etkilememelidir. Eğer böyle bir durum varsa ayrı ayrı hesaplanacak şekilde kodlar 
-	yazılmalıdır
+	printf metodu ile x, X, h, H karakterleri kullanılarak tamsayı türlerine ilişkin değerler hexadecimal olarak 
+	formatlanabilir. Ayrıca o (küçük O) format karakteri ile tamsayı türlerine ilişkin değerler octal oalrak formatlanabilir 
 -----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String [] args)
 	{		
+		java.util.Scanner kb = new java.util.Scanner(System.in);
 		
+		System.out.print("Bir sayı giriniz:");
+		int a = Integer.parseInt(kb.nextLine());				
+		
+		System.out.printf("a = %d%n", a);
+		System.out.printf("a = %x%n", a);
+		System.out.printf("a = %X%n", a);
+		System.out.printf("a = %h%n", a);
+		System.out.printf("a = %H%n", a);
+		System.out.printf("a = %o%n", a);		
 	}
 }
 
