57345aa41eeed895ecb6cc5b348c29dd1b71e469
Oğuz Karan <oguzkaran@csystem.org>
2022-10-01 12:28:14 +0300

    Update

--- src/eclipseworkspace/Sample/src/csd/App.java
+++ src/eclipseworkspace/Sample/src/csd/App.java
@@ -1,17 +1,17 @@
 /*---------------------------------------------------------------------------------------------------------------------	
-	Aritmetik 4 işlem ve mod operatörleri iki operandlı (binary) ve araek (infix) operatörlerdir. Bu operatörlerin
-	ürettikleri değer (product value) işlem sonucunda elde edilen değerdir. Bu operatörlerin yan etkisi (side effect) yoktur
+	-- operatörü tek operandlı ve hem prefix hem de postfix olarak kullanılabilen bir operatördür. Bu operatör ister 
+	prefix ister postfix olarak kullanılsın operandına ilişkin değeri bir azaltır. Yani operandının içerisindeki değerin
+	1 eksiği ile değiştirir. Bu operatörün operandının değişken olması zorunludur
 -----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String [] args)
 	{
-		java.util.Scanner kb = new java.util.Scanner(System.in);
-		System.out.print("İki tane sayı giriniz:");
-		int a = kb.nextInt();
-		int b = kb.nextInt();		
+		int a = 10;
 		
-		System.out.printf("%d %% %d = %d%n", a, b, a % b);
+		a = a + 1;
+		
+		System.out.printf("a = %d%n", a);	
 	}
 }
