4cdaa90d4f7e901233bd00cb465fca05a9aad596
Oğuz Karan <oguzkaran@csystem.org>
2022-10-02 15:14:36 +0300

    Update

--- src/eclipseworkspace/Sample/src/csd/App.java
+++ src/eclipseworkspace/Sample/src/csd/App.java
@@ -1,8 +1,3 @@
-/*---------------------------------------------------------------------------------------------------------------------	
-	Anahtar Notlar: Okunabilirlik/algılanabilirlik açısından tek operandlı operatörleri genel olarak operandına bitişik
-	biçimde yazacağız. İki operandlı operatörleri de özel ama önemli olan bazı operatörler dışında operandları ile 
-	operatör arasında yalnızca bir tane "space" karakteri olacak şekilde yazacağız
------------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
@@ -13,10 +8,12 @@ class App {
 		a = 2;
 		b = 3;
 		
-		c = a++-++b;
+		c = a-- - --b;
+
 		
 		System.out.printf("a = %d%n", a);
 		System.out.printf("b = %d%n", b);
 		System.out.printf("c = %d%n", c);
+		
 	}
 }
