e5ed61db3ed289018ae63a1e3d83f023e038b07b
Oğuz Karan <oguzkaran@csystem.org>
2022-10-22 12:37:16 +0300

    Update

--- src/eclipseworkspace/Sample/src/csd/App.java
+++ src/eclipseworkspace/Sample/src/csd/App.java
@@ -1,76 +1,68 @@
 /*----------------------------------------------------------------------------------------------------------------------
-	Sınıf Çalışması: Parametresi ile aldığı int türden bir sayının Armstrong sayısı olup olmadığını test eden isArmstrong
-	metodunu NumberUtil sınıfı içerisinde yazınız
+	Sınıf Çalışması: Parametresi ile aldığı int türden bir n değeri için n-inci asal sayıyı döndüren getPrime isimli
+	metodu yazınız ve aşağıdaki kod ile test ediniz
+	
 	Açıklamalar:
-		- Bir sayının her basamağının "basamak sayı"-ncı kuvvetleri toplanmı sayının kendisine eşitse bu sayıya Armstrong
-		sayısı denit. Örneğin:
-			153 -> 1 ^ 3 + 5 ^ 3 + 3 ^ 3 = 153
-		
-		- Kuvvet alma işlemi için bir önceki örnekte yazılmış olan pow metodu kullanılacaktır
-		
-		- Negatif sayılar Armstrong sayısı olarak kabul edilmeyecektir
-		
-		- Metodu, 1 basamaklı, 2 basamaklı, 3 basamaklı, 4 basamaklı, 5 basamaklı ve 6 basamaklı Armstrong sayılarını
-		ekrana yazdırarak test ediniz	
+		- isPrime metodunun hızlı versiyonunu kullanınız
+		- n değerinin pozitif olmaması durumu metot içerisinde kontrol edilmeyecektir				
 -----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String [] args)
 	{
-		IsArmstrongTest.run();		
+		GetPrimeTest.run();
 	}
 }
 
-class IsArmstrongTest {
+class GetPrimeTest {
 	public static void run()
-	{
-		for (int n = -10; n <= 999999; ++n)
-			if (NumberUtil.isArmstrong(n))
-				System.out.println(n);
+	{		
+		java.util.Scanner kb = new java.util.Scanner(System.in);		
+
 		
-		System.out.println("Tekrar yapıyor musunuz?");		
+		for (;;) {
+			System.out.print("Bir sayı giriniz:");
+			int n = Integer.parseInt(kb.nextLine());
+			
+			if (n <= 0) {
+				System.out.println("Tekrar yapıyor musunuz?");
+				return;
+			}			
+
+			
+			System.out.printf("%d. asal sayı:%d%n", n, NumberUtil.getPrime(n));		 
+		}
 	}
 }
 
-class NumberUtil {	
-	public static boolean isArmstrong(int val)
+class NumberUtil {
+	public static long getPrime(int n)
 	{
-		return val >= 0 && getDigitsPowSum(val) == val;
+		//TODO:
 	}
 	
-	public static int getDigitsPowSum(int val)
+	public static boolean isPrime(long val)
 	{
-		int n = countDigits(val);
-		int total = 0;
+		if (val <= 1)
+			return false;
 		
-		while (val != 0) {
-			total += pow(val % 10, n);
-			val /= 10;
-		}		
+		if (val % 2 == 0)
+			return val == 2;
 		
-		return total;		
-	}
-	
-	public static int countDigits(int val)
-	{	
-		int count = 0;
+		if (val % 3 == 0)
+			return val == 3;
 		
-		do {
-			++count;
-			val /= 10;
-		} while (val != 0);		
+		if (val % 5 == 0)
+			return val == 5;		
+
+		if (val % 7 == 0)
+			return val == 7;
 		
-		return count;
-	}
-	
-	public static int pow(int a, int b)
-	{	
-		int result = 1;
+		for (long i = 11; i * i <= val; i += 2)
+			if (val % i == 0)
+				return false;
 		
-		while (b-- > 0)
-			result *= a;
-		
-		return result;
+		return true;
 	}
 }
