81ae7c83167bc81fa4719c0b6d99c54281e9db56
Oğuz Karan <oguzkaran@csystem.org>
2022-10-22 10:37:25 +0300

    Update

--- src/eclipseworkspace/Sample/src/csd/App.java
+++ src/eclipseworkspace/Sample/src/csd/App.java
@@ -1,18 +1,76 @@
 /*----------------------------------------------------------------------------------------------------------------------
-	Hiç sonlanmayacak sanılan ancvak öyle olmayan bir döngü örneği. Örnekte i değeri azalarak -2147483648 değerine 
-	yani int türünün en küçük değerine geldiğinde artık bir azaltıldığında 2147483647 değeri yani int türünün en büyük
-	değeri elde edilir ve döngüden çıkılır
+	Sınıf Çalışması: Parametresi ile aldığı int türden bir sayının Armstrong sayısı olup olmadığını test eden isArmstrong
+	metodunu NumberUtil sınıfı içerisinde yazınız
+	Açıklamalar:
+		- Bir sayının her basamağının "basamak sayı"-ncı kuvvetleri toplanmı sayının kendisine eşitse bu sayıya Armstrong
+		sayısı denit. Örneğin:
+			153 -> 1 ^ 3 + 5 ^ 3 + 3 ^ 3 = 153
+		
+		- Kuvvet alma işlemi için bir önceki örnekte yazılmış olan pow metodu kullanılacaktır
+		
+		- Negatif sayılar Armstrong sayısı olarak kabul edilmeyecektir
+		
+		- Metodu, 1 basamaklı, 2 basamaklı, 3 basamaklı, 4 basamaklı, 5 basamaklı ve 6 basamaklı Armstrong sayılarını
+		ekrana yazdırarak test ediniz	
 -----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String [] args)
 	{
-		int i;
+		IsArmstrongTest.run();		
+	}
+}
+
+class IsArmstrongTest {
+	public static void run()
+	{
+		for (int n = -10; n <= 999999; ++n)
+			if (NumberUtil.isArmstrong(n))
+				System.out.println(n);
 		
-		for (i = 0; i < 10; --i)
-			System.out.println(i);
+		System.out.println("Tekrar yapıyor musunuz?");		
+	}
+}
+
+class NumberUtil {	
+	public static boolean isArmstrong(int val)
+	{
+		return val >= 0 && getDigitsPowSum(val) == val;
+	}
+	
+	public static int getDigitsPowSum(int val)
+	{
+		int n = countDigits(val);
+		int total = 0;
 		
-		System.out.printf("i = %d%n", i);
+		while (val != 0) {
+			total += pow(val % 10, n);
+			val /= 10;
+		}		
+		
+		return total;		
+	}
+	
+	public static int countDigits(int val)
+	{	
+		int count = 0;
+		
+		do {
+			++count;
+			val /= 10;
+		} while (val != 0);		
+		
+		return count;
+	}
+	
+	public static int pow(int a, int b)
+	{	
+		int result = 1;
+		
+		while (b-- > 0)
+			result *= a;
+		
+		return result;
 	}
 }
