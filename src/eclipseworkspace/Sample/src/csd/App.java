9e04f6d8f6424cce40c60b130360de4f02894356
Oğuz Karan <oguzkaran@csystem.org>
2022-10-23 10:28:59 +0300

    Update

--- src/eclipseworkspace/Sample/src/csd/App.java
+++ src/eclipseworkspace/Sample/src/csd/App.java
@@ -1,68 +1,46 @@
 /*----------------------------------------------------------------------------------------------------------------------
-	Sınıf Çalışması: Parametresi ile aldığı int türden bir n değeri için n-inci asal sayıyı döndüren getPrime isimli
-	metodu yazınız ve aşağıdaki kod ile test ediniz
-	
+	Sınıf Çalışması: Parametresi ile aldığı int türden bir sayının faktoriyel değerini döndüren factorial isimli metodu
+	NumberUtil sınıfı içerisinde yazını ve aşağıdaki kod ile test ediniz.
 	Açıklamalar:
-		- isPrime metodunun hızlı versiyonunu kullanınız
-		- n değerinin pozitif olmaması durumu metot içerisinde kontrol edilmeyecektir				
+		- Faktoriyel işleni aşağıdaki gibidir: 
+			0! = 1
+			1! = 1
+			2! = 1 * 2
+			3! = 1 * 2 * 3
+			
+			...
+			
+			n! = 1 * 2 * ... * (n - 1) * n
+		- Negatif sayılar için faktoriyel değeri 1 olarak döndürülecektir	
 -----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String [] args)
 	{
-		GetPrimeTest.run();
+		FactorialTest.run();
 	}
 }
 
-class GetPrimeTest {
+class FactorialTest {
 	public static void run()
 	{		
 		java.util.Scanner kb = new java.util.Scanner(System.in);		
 
+		System.out.print("Bir sayı giriniz:");
+		int val = kb.nextInt();
 		
-		for (;;) {
-			System.out.print("Bir sayı giriniz:");
-			int n = Integer.parseInt(kb.nextLine());
-			
-			if (n <= 0) {
-				System.out.println("Tekrar yapıyor musunuz?");
-				return;
-			}			
-
-			
-			System.out.printf("%d. asal sayı:%d%n", n, NumberUtil.getPrime(n));		 
-		}
+		for (int n = 0; n <= val; ++n)
+			System.out.printf("%d! = %d%n", n, NumberUtil.factorial(n));
+		
+		System.out.println("Tekrar yapıyor musunuz?");
 	}
 }
 
 class NumberUtil {
-	public static long getPrime(int n)
+	public static long factorial(int n)
 	{
-		//TODO:
-	}
-	
-	public static boolean isPrime(long val)
-	{
-		if (val <= 1)
-			return false;
-		
-		if (val % 2 == 0)
-			return val == 2;
-		
-		if (val % 3 == 0)
-			return val == 3;
-		
-		if (val % 5 == 0)
-			return val == 5;		
-
-		if (val % 7 == 0)
-			return val == 7;
-		
-		for (long i = 11; i * i <= val; i += 2)
-			if (val % i == 0)
-				return false;
-		
-		return true;
-	}
+		//TODO:		
+	}	
 }
+
