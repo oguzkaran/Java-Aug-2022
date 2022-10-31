bbdbdaf49bba17ebe7e4bdf196d935173d889619
Oğuz Karan <oguzkaran@csystem.org>
2022-10-31 11:23:13 +0300

    Update

--- src/eclipseworkspace/Sample/src/csd/App.java
+++ src/eclipseworkspace/Sample/src/csd/App.java
@@ -1,5 +1,11 @@
 /*----------------------------------------------------------------------------------------------------------------------
+	Sınıf Çalışması: Parametresi ile aldığı n sayısı için n-inci Fibonacci sayısını döndüren getFibonacciNumber isimli
+	metodu NumberUtil sınıfı içerisinde yazınız ve aşağıdaki kod ile test ediniz
 	
+	Açıklamalar:
+		- Fibonacci sayıları
+			0 1 1 2 3 5 8 13 21 ...
+		- Metot pozitif olmayan n değerleri için kontrol yapmayacaktır
 -----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
@@ -9,5 +15,3 @@ class App {
 		
 	}
 }
-
-
