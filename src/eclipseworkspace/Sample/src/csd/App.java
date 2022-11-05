933590f653df386b06bc1fd575c3f4afa32f682e
Oğuz Karan <oguzkaran@csystem.org>
2022-11-05 10:24:31 +0300

    Update

--- src/eclipseworkspace/Sample/src/csd/App.java
+++ src/eclipseworkspace/Sample/src/csd/App.java
@@ -1,17 +1,31 @@
 /*----------------------------------------------------------------------------------------------------------------------
-	Sınıf Çalışması: Parametresi ile aldığı n sayısı için n-inci Fibonacci sayısını döndüren getFibonacciNumber isimli
-	metodu NumberUtil sınıfı içerisinde yazınız ve aşağıdaki kod ile test ediniz
-	
-	Açıklamalar:
-		- Fibonacci sayıları
-			0 1 1 2 3 5 8 13 21 ...
-		- Metot pozitif olmayan n değerleri için kontrol yapmayacaktır
+	Anahtar Notlar: switch deyiminin bölümleri içerisinde (case ve default) çok fazla işlem yapılması okunabilirlik/algılanabilirlik
+	açısından iyi bir teknik değildir. Bu durumda metot yazıp ilgili bölümlerde çağırmak gerekir. Bunun için net bir sınır
+	olmasa da 3-4 satırı geçen kodların yazılamaması gerekir 
 -----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String [] args)
 	{
+		java.util.Scanner kb = new java.util.Scanner(System.in);
 		
+		System.out.print("Posta kodunu giriniz:");
+		int postalCode = kb.nextInt();
+		
+		switch (postalCode) {		
+		case 67100:
+			System.out.println("Zonguldak Bahçelievler Mahallesi");
+			break;
+		case 34387:
+			System.out.println("İstanbul Mecidiyeköy");
+			break;
+		default:
+			System.out.println("Geçersiz posta kodu");	
+			break;
+		}
+		
+		System.out.println("Tekrar yapıyor musunuz?");
 	}
 }
+
