788488382e48e854aff6cc29917658d2d9632f94
Oğuz Karan <oguzkaran@csystem.org>
2022-11-05 11:56:30 +0300

    Update

--- src/eclipseworkspace/Sample/src/csd/App.java
+++ src/eclipseworkspace/Sample/src/csd/App.java
@@ -1,31 +1,50 @@
 /*----------------------------------------------------------------------------------------------------------------------
-	Anahtar Notlar: switch deyiminin bölümleri içerisinde (case ve default) çok fazla işlem yapılması okunabilirlik/algılanabilirlik
-	açısından iyi bir teknik değildir. Bu durumda metot yazıp ilgili bölümlerde çağırmak gerekir. Bunun için net bir sınır
-	olmasa da 3-4 satırı geçen kodların yazılamaması gerekir 
+	Sınıf Çalışması: Parametresi ile int türden gün, ay ve yıl bilgilerine ilişkin tarihin geçerli olup olmadığını 
+	test eden isValidDate isimli metodu yazını ve aşağıdaki kod ile test ediniz
+	(İleride daha iyisi yazılacaktır)
 -----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String [] args)
 	{
-		java.util.Scanner kb = new java.util.Scanner(System.in);
-		
-		System.out.print("Posta kodunu giriniz:");
-		int postalCode = kb.nextInt();
-		
-		switch (postalCode) {		
-		case 67100:
-			System.out.println("Zonguldak Bahçelievler Mahallesi");
-			break;
-		case 34387:
-			System.out.println("İstanbul Mecidiyeköy");
-			break;
-		default:
-			System.out.println("Geçersiz posta kodu");	
-			break;
-		}
-		
-		System.out.println("Tekrar yapıyor musunuz?");
+		IsValidDateTest.run();
 	}
 }
 
+class IsValidDateTest {
+	public static void run()
+	{
+		java.util.Scanner kb = new java.util.Scanner(System.in);
+		
+		for (;;) {
+			System.out.println("Gün ay ve yıl bilgilerini giriniz:");
+			int day = kb.nextInt();
+			int month = kb.nextInt();
+			int year = kb.nextInt();
+			
+			if (day == 0)
+				break;
+			
+			if (DateUtil.isValidDate(day, month, year))
+				System.out.printf("%02d/%02d/%04d tarihi geçerlidir%n", day, month, year);
+			else
+				System.out.println("Geçersiz tarih");
+
+		}
+		
+		System.out.println("Tekrar yapıyor munuz?");
+	}
+}
+
+class DateUtil {
+	public static boolean isValidDate(int day, int month, int year)
+	{
+		//TODO:
+	}
+	
+	public static boolean isLeapYear(int year)
+	{
+		return year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
+	}
+}
