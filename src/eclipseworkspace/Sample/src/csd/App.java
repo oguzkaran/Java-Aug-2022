34d24fd5186af8a5e333c842dbee1da7bf07d756
Oğuz Karan <oguzkaran@csystem.org>
2022-11-19 10:39:35 +0300

    Update

--- src/eclipseworkspace/Sample/src/csd/App.java
+++ src/eclipseworkspace/Sample/src/csd/App.java
@@ -1,56 +1,44 @@
 /*----------------------------------------------------------------------------------------------------------------------
-	Java 12 ile birlikte "preview" olarak "switch expression" dile dahil edilmiştir. Bu durumda switch ifade biçiminde de 
-	kullanılabilecek şekilde sentaks ve semantik açıdan geliştirilmiştir. switch'in bu şekildeki kullanımı
-	"switch expression statement" olarak da adlandırılabilir. Yani kabaca switch expression semantiği statement biçiminde de
-	kullanılabilir. 
+	Aşağıdaki örnekte switch expression, statement biçiminde kullanılmıştır. switch statement karşılığı şu şekildedir:
+	switch (code) {
+		case 67100:
+			System.out.println("Zonguldak Bahçelievler");
+			break;
+		case 34387:
+			System.out.println("İstanbul Mecidiyeköy");
+			break;
+		default:
+			System.out.println("Geçersiz post kodu");				
+	}
+	
+	switch expression'ın fall-through özelliği yoktur
 -----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String [] args)
 	{
-		PrintDiamondApp.run();
-	}
-}
-
-
-class PrintDiamondApp {
-	public static void printAbove(int n)
-	{
-		for (int i = 1; i < n; ++i) {
-			for (int k = 0; k < n - i; ++k)
-				System.out.print(' ');
-			
-			for (int k = 0; k < 2 * i - 1; ++k)
-				System.out.print('*');
-			
-			System.out.println();			
-		}		
-	}
-	
-	public static void printBelow(int n)
-	{
-		for (int i = 0; i < n; ++i) {
-			for (int k = 0; k < i; ++k)
-				System.out.print(' ');
-			
-			for (int k = 0; k < 2 * (n - i) - 1; ++k)
-				System.out.print('*');
-			
-			System.out.println();			
-		}		
-	}
-	
-	public static void printDiamond(int n)
-	{
-		printAbove(n);
-		printBelow(n);
-	}
-		
-	public static void run()
-	{
 		java.util.Scanner kb = new java.util.Scanner(System.in);
-		System.out.print("Bir sayı giriniz:");
-		printDiamond(kb.nextInt());
+		
+		
+		for (;;) {
+			System.out.print("Posta kodunu giriniz:");
+			int code = Integer.parseInt(kb.nextLine());
+			
+			if (code <= 0)
+				break;
+			
+			switch (code) {
+			case 67100 -> System.out.println("Zonguldak Bahçelievler");
+				
+			case 34387 -> System.out.println("İstanbul Mecidiyeköy");
+				
+			default -> System.out.println("Geçersiz post kodu");				
+			}
+		}
+		
+		System.out.println("Tekrar yapıyor musunuz?");
 	}
 }
+
+
