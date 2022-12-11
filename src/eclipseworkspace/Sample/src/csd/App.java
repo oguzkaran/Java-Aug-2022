4d1ba3ce230df88ed05a0b13f3aeb1c111c9fc02
Oğuz Karan <oguzkaran@csystem.org>
2022-12-11 10:42:10 +0300

    Update

--- src/eclipseworkspace/Sample/src/csd/App.java
+++ src/eclipseworkspace/Sample/src/csd/App.java
@@ -1,30 +1,60 @@
 /*----------------------------------------------------------------------------------------------------------------------
-	Bir nesnenin bellekte kapladığı "en az non-static veri elemanlarının toplam uzunluğu kadardır". Bu durumda aynı 
-	türden nesneler için aynı uzunlukta yer ayrılır. Farklı türden nesneler için farklı veri elemanları olabileceğinden
-	aynı uzunlukta yer ayrılması gerekmez. 
+	Bir metodun geri dönüş değeri bir referans olabilir. Bu durumda bu metodun geri dönüş değerine ilişkin return deyimine
+	ait ifadenin türünün aynı türden bir referans olması gerekir. Bir metodun parametre değişkeni bir referans olabilir.
+	Bu durumda metot aynı türden bir referans ile çağrılabilir. Metoda geçilen referans ile metot içerisinde ilgili nesneye
+	erişilmiş olur. Referans parametreli bir metot aldığı referansa ilişkin nesne üzerinde değişiklik yapabilir. Bu durumda
+	bu değişiklik metodu çağıran koddaki referans üzerinden yine aynı nesneye erişildiği için görülebilir.
 	
-	Türü ne olursa olsun tüm referansların uzunlukları aynıdır. Bu da adreslenebilecek sayıları tutabilecek uzunlukturç 
-	Örneğin 64 bit sistemlerde tüm referanslar 64 bit (8 byte), 32 bit sistemlerde ise 32 bit (4 byte) uzunluğundadır
+	Aşağıdaki örnekte createDate metodu içeride yaratmış olduğu nesnenin referansını geri dönüş değeri olarak vermektedir.
+	printDate metodu parametresi ile aldığı Date türden referansın gösterdiği nesneye erişip bilgilerini ekrana yazmaktadır.
+	changeDate metodu aldığı referansa ilişkin nesneye erişerek elemanlarını yine parametresi ile aldığı değerler ile
+	değiştirmektedir. Şüphesiz createDate ve changeDate metotları tarih kontrollerini de yapacaktır. Örnekte bu durum
+	//... biçiminde gösterilmiştir. Örneği çalıştırarak durumu gözlemleyiniz
 -----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String [] args)
-	{	
-		Sample s = new Sample();
-		Sample k = new Sample();		
+	{		
+		Date date = DateUtil.createDate(11, 7, 1983);
 		
-		System.out.printf("s.val = %d%n", s.val);
-		System.out.printf("s.flag = %b%n", s.flag);
-		System.out.printf("k.val = %d%n", k.val);
-		System.out.printf("k.flag = %b%n", k.flag);
+		DateUtil.printDate(date);		
+		DateUtil.changeDate(date, 6, 9, 2021);
+		DateUtil.printDate(date);
 	}
 }
 
-class Sample {
-	public int val;
-	public boolean flag;
+class DateUtil {
+	public static void changeDate(Date date, int day, int month, int year)
+	{
+		//...
+		
+		date.day = day;
+		date.month = month;
+		date.year = year;
+	}
 	
-	//...
+	public static Date createDate(int day, int month, int year)
+	{
+		//...
+		
+		Date date = new Date();
+		
+		date.day = day;
+		date.month = month;
+		date.year = year;
+		
+		return date;				
+	}
+	
+	public static void printDate(Date date)
+	{
+		System.out.printf("%02d/%02d/%04d%n", date.day, date.month, date.year);		
+	}
 }
 
+class Date {
+	public int day, month, year;
+
+	//...
+}
