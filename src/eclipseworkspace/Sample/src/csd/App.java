cb7eedf02d4b753a2df2ca04077230ec85e41b87
Oğuz Karan <oguzkaran@csystem.org>
2022-12-24 16:54:03 +0300

    Update

--- src/eclipseworkspace/Sample/src/csd/App.java
+++ src/eclipseworkspace/Sample/src/csd/App.java
@@ -1,5 +1,10 @@
 /*----------------------------------------------------------------------------------------------------------------------
-	Random sınıfında Java 17 ile birlikte dolaylı olarak iki tane int türden parametreli bir nextInt metodu da buklunmaktadır	
+	Aşağıdaki örnekte resgele üretilen ilk 10 sayının dizilimi her çalıştırmada farklı olur. İlk 10 sayının diziliminin
+	aynı gelme olasılığı düşüktür. İkinci üretilen 10 sayı klavyeden alınan tohum değerine göre üretilir. Bu durumda 
+	örneğin her çalıştırmada tohum değeri 200 olarak girilirse ikinci üretilen 10 saynın dizilimi hep aynı olacaktır. 
+	Programı çalıştırıp sonuçları gözlemleyiniz 
+	
+	seed = seed * 123456 *  23456;
 -----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
@@ -7,11 +12,21 @@ class App {
 	public static void main(String [] args)
 	{
 		java.util.Random r = new java.util.Random();
+		java.util.Scanner kb = new java.util.Scanner(System.in);
 		
-		for (int i = 0; i < 15; ++i)
-			System.out.printf("%d ", r.nextInt(10, 21));//[10, 20]
+		System.out.println("Bir sayı giriniz:");
+		long seed = kb.nextLong();		
+		
+		for (int i = 0; i < 10; ++i)
+			System.out.printf("%d ", r.nextInt(100));	
+		
+		System.out.println();
+		
+		r.setSeed(seed);
+		
+		for (int i = 0; i < 10; ++i)
+			System.out.printf("%d ", r.nextInt(100));	
 		
 		System.out.println();
 	}
 }
-
