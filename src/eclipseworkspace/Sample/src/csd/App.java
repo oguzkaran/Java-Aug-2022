139f27ec81fb7d899fdf4643d7a7209ada56d337
Oğuz Karan <oguzkaran@csystem.org>
2023-01-07 12:51:15 +0300

    Update

--- src/eclipseworkspace/Sample/src/csd/App.java
+++ src/eclipseworkspace/Sample/src/csd/App.java
@@ -1,16 +1,22 @@
 /*----------------------------------------------------------------------------------------------------------------------
-	String sınıfı: 
-	Programlamada yazılarla işlem her uygulamada yapılır. Java'da yazılarla işlem yapmakta kullanılan en temel sınıf 
-	String sınıfıdır. Yazılarla işlem yapan başka sınıflar da vardır. Bunların hemen hemen hepsi aslında String sınıfına
-	yardımcı sınıflardır. String sınıfı java.lang paketi içerisinde bulunduğu için ismine doğrudan erişilebilir. String
-	"primitive" bir tür değildir. JavaSE'de bulunan bir sınıftır.  
+	Derleyici özdeş "string literal"'lar için aynı nesnenin referansını verecek bir kod üretirler. Yani bir string literal
+	ile özdeş kaç tane sabit olursa olsun hepsi için aynı nesnenin referansı elde edilir. Yeniden nesne yaratılmaz. Aşağıdaki
+	örnekte yine referans karşılaştırması yapılmıştır. Ancak s1 ve s2'ye atanan string literal'lar özdeş olduğundan aynı
+	adresler atanmış olur. Örnek durumu anlatmak için yazılmıştır.
+	
+	Anahtar Notlar: Derleyicinin ödeş string'ler için aynı adresi vermesi String sınıfının immutable olmasındandır. Aksi
+	olsaydı yani String sınıfı immutable olmasaydı aynı adres verildiğinde herhangi bir referans üzerinden değişiklik
+	yapılması durumunda diğer referans üzerinden de değişiklik görülebilirdi. Bu durumda String sınıfı kullanan programcı
+	açısından karmaşık durumlar oluşabilirdi 
 -----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String [] args)
-	{
+	{		
+		String s1 = "cemil";		
+		String s2 = "cemil";
 		
+		System.out.println(s1 == s2 ? "Aynı yazı" : "Farklı yazılar");				
 	}
 }
-
