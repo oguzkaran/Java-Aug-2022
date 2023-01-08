4c7ab9467e0aee6289f5492ac1d7bb5d1c615983
Oğuz Karan <oguzkaran@csystem.org>
2023-01-08 09:28:38 +0300

    Update

--- src/eclipseworkspace/Sample/src/csd/App.java
+++ src/eclipseworkspace/Sample/src/csd/App.java
@@ -1,22 +1,56 @@
 /*----------------------------------------------------------------------------------------------------------------------
-	Derleyici özdeş "string literal"'lar için aynı nesnenin referansını verecek bir kod üretirler. Yani bir string literal
-	ile özdeş kaç tane sabit olursa olsun hepsi için aynı nesnenin referansı elde edilir. Yeniden nesne yaratılmaz. Aşağıdaki
-	örnekte yine referans karşılaştırması yapılmıştır. Ancak s1 ve s2'ye atanan string literal'lar özdeş olduğundan aynı
-	adresler atanmış olur. Örnek durumu anlatmak için yazılmıştır.
-	
-	Anahtar Notlar: Derleyicinin ödeş string'ler için aynı adresi vermesi String sınıfının immutable olmasındandır. Aksi
-	olsaydı yani String sınıfı immutable olmasaydı aynı adres verildiğinde herhangi bir referans üzerinden değişiklik
-	yapılması durumunda diğer referans üzerinden de değişiklik görülebilirdi. Bu durumda String sınıfı kullanan programcı
-	açısından karmaşık durumlar oluşabilirdi 
+	Sınıf Çalışması: Parametresi ile aldığı iki yazıdan birincisi içerisinde ikincisinden kaç tane olduğunu döndüren 
+	countString isimli metodu ve büyük -küçük harf duyarsız olarak çalışan countStringIgnoreCase isimli metodu
+	yazınız ve aşağıdaki kod ile test ediniz.
+	Örneğin
+	aaa yazısı içerisinde aa'dan 2 tane vardır
 -----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String [] args)
-	{		
-		String s1 = "cemil";		
-		String s2 = "cemil";
+	{
+		CountStringTest.run();
+	}
+}
+
+class CountStringTest {
+	public static void run()
+	{
+		java.util.Scanner kb = new java.util.Scanner(System.in);
 		
-		System.out.println(s1 == s2 ? "Aynı yazı" : "Farklı yazılar");				
+		for (;;) {
+			System.out.print("Birinci yazıyı giriniz:");
+			String s1 = kb.nextLine();
+			
+			if ("elma".equals(s1))
+				break;
+			
+			System.out.print("İkinci yazıyı giriniz:");
+			String s2 = kb.nextLine();
+			
+			int count = StringUtil.countString(s1, s2);
+			
+			System.out.printf("Birinci yazıda ikinci yazıdan %d adet var%n", count);
+			count = StringUtil.countStringIgnoreCase(s1, s2);
+			
+			System.out.printf("Birinci yazıda ikinci yazıdan \"case insensitive\" olarak %d adet var%n", count);
+		}
+		
+		
+		System.out.println("Tekrar yapıyor musunuz?");
+	}
+}
+
+
+class StringUtil {
+	public static int countString(String s1, String s2)
+	{
+		//TODO:
+	}
+	
+	public static int countStringIgnoreCase(String s1, String s2)
+	{
+		//TODO:
 	}
 }
