5c1ade4bf7002a12351d0f168ddbb1fa9b760582
Oğuz Karan <oguzkaran@csystem.org>
2023-01-14 12:44:34 +0300

    Update

--- src/eclipseworkspace/Sample/src/csd/App.java
+++ src/eclipseworkspace/Sample/src/csd/App.java
@@ -1,46 +1,79 @@
 /*----------------------------------------------------------------------------------------------------------------------
-	Sınıf Çalışması: Parametresi ile aldığı bir yazının sadece başındaki boşluk (whitespace) karakterlerini atan trimLeading
-	ve sadece sonundaki boşluk karakterlerini atan trimTrailing isimli metotları StringUtil sınıfı içerisinde yazınız
-	ve aşağıdaki kod ile test ediniz  
----------------------------------------------------------------------------------------------------------------------*/
+	Sınıf Çalışması: Aşağıda açıklanan metotları StringUtil sınıfı içerisinde yazınız ve aşağıdaki kod ile test ediniz.
+	
+	public static String padLeading(String s, int len, char ch);
+	public static String padLeading(String s, int len);
+	public static String padTrailing(String s, int len, char ch);
+	public static String padTrailing(String s, int len);
+	
+	Açıklamalar:
+	- padLeading metodunun 3 parametreli overload'u ikinci parametresi ile aldığı len, birinci paramtresi ile aldığı
+	yazının uzunluğuğundan büğyükse yazıyı soldan (baştan) üçüncü parametresi ile aldığı karakter ile dolduracaktır.
+	Örneğin:
+		padLeading("csd", 5, 'x'); 
+	çağrısı
+		xxcsd
+	yazısını döndürecektir
+	
+	- len uzunluğu yazının uzunluğundan küçük veya eşitse aynı referansa geri dönecektir
+	
+	- padLeading metodunun 2 parametreli overload'u yazıyı soldan (baştan) space karakteri ile dolduracaktır
+	
+	- padTrailing metotları benzer şekilde sağdan (sondan) dolduracak şekilde yazılacaktır
+	
+	- Örneği Java 11 öncesi için yazınız
+-----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String [] args)
 	{
-		TrimLeadingTrailingTest.run();
+		PadLeadingTrailingTest.run();
 	}
 }
 
-class TrimLeadingTrailingTest {
+class PadLeadingTrailingTest {
 	public static void run()
 	{
-		java.util.Scanner kb = new java.util.Scanner(System.in);
+		java.util.Scanner kb = new java.util.Scanner(System.in);		
 		
 		for (;;) {
 			System.out.print("Bir yazı giriniz:");
-			String s = kb.nextLine();
-			
-			System.out.printf("(%s)%n", s);
-			System.out.printf("(%s)%n", StringUtil.trimLeading(s));
-			System.out.printf("(%s)%n", StringUtil.trimTrailing(s));
+			String s = kb.nextLine();			
 			
 			if ("elma".equals(s))
 				break;
+			
+			System.out.print("Bir sayı giriniz:");
+			int count = Integer.parseInt(kb.nextLine());
+			
+			System.out.printf("(%s)%n", StringUtil.padLeading(s, count, 'x'));
+			System.out.printf("(%s)%n", StringUtil.padLeading(s, count));
+			System.out.printf("(%s)%n", StringUtil.padTrailing(s, count, 'x'));
+			System.out.printf("(%s)%n", StringUtil.padTrailing(s, count));			
 		}
 		
 		System.out.println("Tekrar yapıyor musunuz?");
 	}
 }
 
-
 class StringUtil {
-	public static String trimLeading(String s)
+	public static String padLeading(String s, int len, char ch)
 	{
 		//TODO:
 	}
 	
-	public static String trimTrailing(String s)
+	public static String padLeading(String s, int len)
+	{
+		//TODO:
+	}
+	
+	public static String padTrailing(String s, int len, char ch)
+	{
+		//TODO:
+	}
+	
+	public static String padTrailing(String s, int len)
 	{
 		//TODO:
 	}
