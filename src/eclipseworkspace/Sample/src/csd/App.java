37156daf7a3e7dfd28f6e82d1ecaa54bfd54dc99
Oğuz Karan <oguzkaran@csystem.org>
2023-01-15 11:14:40 +0300

    Update

--- src/eclipseworkspace/Sample/src/csd/App.java
+++ src/eclipseworkspace/Sample/src/csd/App.java
@@ -1,37 +1,74 @@
 /*----------------------------------------------------------------------------------------------------------------------
-	String sınıfının compareToIgnoreCase metodu "case insensitive" karşılaştırma yapar
+	Sınıf Çalışması: Parametresi ile aldığı bir yazının Türkçe pangram olup olmadığını test eden isPangramTR ve 
+	İngilizce pangram olup olmadığını test eden isPangramEN metotlarını StringUtil sınıfı içerisinde yazınız ve aşağıdaki
+	kod ile test ediniz
+	Pangram: İçerisinde özel isim olmayan ve ilgili dilin alfabesindeki tüm karakterleri içeren anlamlı cümlelere denir.
+	
+	Programda özel isim ve anlam dikkate alınmayacaktır
+	
+	İnglizce: The quick brown fox jumps over the lazy dog
+	Türkçe: Pijamalı hasta yağız şoföre çabucak güvendi
 -----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String [] args)
 	{
-		java.util.Scanner kb = new java.util.Scanner(System.in);
-		
-		for (;;) {
-			System.out.print("Birinci yazıyı giriniz:");
-			String s1 = kb.nextLine();
-			
-			if ("elma".equals(s1))
-				break;
-			
-			System.out.print("İkinci yazıyı giriniz:");
-			String s2 = kb.nextLine();
-			
-			int result = s1.compareToIgnoreCase(s2);
-			
-			System.out.printf("Result:%d%n", result);
-			
-			if (result < 0)
-				System.out.printf("'%s', '%s' yazısından önce gelir%n", s1, s2);
-			else if (result > 0)
-				System.out.printf("'%s', '%s' yazısından önce gelir%n", s2, s1);
-			else
-				System.out.printf("'%s', '%s' aynı yazılardır%n", s1, s2);
-		}
-		
-		System.out.println("Tekrar yapıyor musunuz?");
+		IsPangramTest.run();
 	}
 }
 
+class IsPangramTest {
+	public static void run()
+	{
+		java.util.Scanner kb = new java.util.Scanner(System.in);
+		
+		IsPangramTestTR.run(kb);
+		IsPangramTestEN.run(kb);
+	}
+}
+
+
+
+class IsPangramTestTR {
+	public static void run(java.util.Scanner kb)
+	{
+		for (;;) {
+			System.out.print("Bir yazı giriniz:");
+			String s = kb.nextLine();
+			
+			if ("elma".equals(s))
+				break;
+			
+			System.out.println(StringUtil.isPangramTR(s) ? "Pangram" : "Pangram değil");
+		}
+	}
+}
+
+class IsPangramTestEN {
+	public static void run(java.util.Scanner kb)
+	{
+		for (;;) {
+			System.out.print("Input a text:");
+			String s = kb.nextLine();
+			
+			if ("quit".equals(s))
+				break;
+			
+			System.out.println(StringUtil.isPangramEN(s) ? "Pangram" : "Not a Pangram");
+		}
+	}
+}
+
+class StringUtil {
+	public static boolean isPangramTR(String s)
+	{
+		//TODO:
+	}
+	
+	public static boolean isPangramEN(String s)
+	{
+		//TODO:
+	}
+}
 
