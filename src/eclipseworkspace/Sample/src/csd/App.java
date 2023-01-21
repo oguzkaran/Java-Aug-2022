03e62b761cb4be603980a7645d937f6f2c84b036
Oğuz Karan <oguzkaran@csystem.org>
2023-01-21 12:36:56 +0300

    Update

--- src/eclipseworkspace/Sample/src/csd/App.java
+++ src/eclipseworkspace/Sample/src/csd/App.java
@@ -1,46 +1,21 @@
 /*----------------------------------------------------------------------------------------------------------------------
-	Sınıf Çalışması: Parametresi ile aldığı bir yazının palindrom olup olmadığını test eden isPalindrome isimli metodu 
-	yazınız ve aşağıdaki kod ile test ediniz.
+	Paketler (Packages) ve İsim Arama (Name Lookup):
 	
-	Palindrom: Yalnızca alfabetik karakterleri tersten okunduğunda aynı olan yazılara denir. Detaylar gözardı edilerek
-	bu tanım yapılabilir
-	
-	Örnekler:
-	Ey Edip Adana'da pide ye	-> eyedipadanadapideye
-	Anastas mum satsana 		-> anastasmumsatsana
-	Ali Papila 					-> alipapila
-	
-	Not: İleride daha iyisi yazılacaktır
 -----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String [] args)
 	{
-		IsPalindromeTest.run();
-	}
-}
-
-class IsPalindromeTest {
-	public static void run()
-	{		
 		java.util.Scanner kb = new java.util.Scanner(System.in);
+		System.out.print("Şehir ismini giriniz:");
+		String city = kb.nextLine();
 		
-		for (;;) {
-			System.out.print("Bir yazı giriniz:");
-			String s = kb.nextLine();
-			
-			if ("elma".equals(s))
-				break;
-			
-			System.out.printf(StringUtil.isPalindrome(s) ? "Palindrom" : "Palindrom değilk");			
-		}		
+		switch (city.toLowerCase()) {
+		case "edirne", "istanbul", "balıkesir" -> System.out.println("Marmara bölgesi");			
+		case "zonguldak", "sinop", "bolu" -> System.out.println("Batı karadeniz bölgesi");			
+		default -> System.out.println("Geçersiz şehir ismi");
+		}
 	}
 }
 
-class StringUtil {	
-	public static boolean isPalindrome(String s)
-	{
-		//TODO:
-	}
-}
