362d3dd0274429a4a204ddb5c948c6d3a368a3db
Oğuz Karan <oguzkaran@csystem.org>
2023-02-05 12:39:57 +0300

    Update

--- src/Sample/Sample/src/org/csystem/app/App.java
+++ src/Sample/Sample/src/org/csystem/app/App.java
@@ -1,7 +1,32 @@
+/*----------------------------------------------------------------------------------------------------------------------
+	Bir dizi yaratıldığında tüm elemanlarına default değerler atanır
+-----------------------------------------------------------------------------------------------------------------------*/
 package org.csystem.app;
+
+import java.util.Random;
+import java.util.Scanner;
+
 class App {
-    public static void main(String[] args)
-    {
-        System.out.println("Hello, World");
-    }
+	public static void main(String [] args) 
+	{
+		Scanner kb = new Scanner(System.in);
+		Random r = new Random();
+
+		System.out.print("Bir sayı giriniz:");
+		int count = kb.nextInt();
+
+		int [] a = new int[count];
+		boolean [] b = new boolean[count];
+
+		for (int i = 0; i < count; ++i)
+			System.out.printf("%d ", a[i]);
+
+		System.out.println();
+
+		for (int i = 0; i < count; ++i)
+			System.out.printf("%b ", b[i]);
+
+		System.out.println();
+	}
 }
+
