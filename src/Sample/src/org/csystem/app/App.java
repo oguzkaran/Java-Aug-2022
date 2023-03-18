101fa7c905f2cbcaadc2d2d737b237e8db333c99
Oğuz Karan <oguzkaran@csystem.org>
2023-03-18 11:54:52 +0300

    Update

--- src/Sample/src/org/csystem/app/App.java
+++ src/Sample/src/org/csystem/app/App.java
@@ -1,19 +1,27 @@
 /*----------------------------------------------------------------------------------------------------------------------
-
+	Sınıfın non-static ve final veri elemanlarının sabit ifadesi ile ilkdeğerlenmesi gerekmez
 -----------------------------------------------------------------------------------------------------------------------*/
 package org.csystem.app;
+
+import java.util.Random;
 
 class App {
 	public static void main(String [] args)
 	{
+		Sample s = new Sample();
+		Sample k = new Sample();
 
+		System.out.printf("s.x = %d%n", s.x);
+		System.out.printf("k.x = %d%n", k.x);
 	}
 }
 
 class Sample {
-	public void foo()
+	private static int randomValue()
 	{
-
+		return new Random().nextInt(0, 100);
 	}
-}
+	public final int x = randomValue();
 
+	//...
+}
