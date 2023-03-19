a93d00c5568ee2d8346cd2e8a35a5322f5d1a5c2
Oğuz Karan <oguzkaran@csystem.org>
2023-03-19 18:14:21 +0300

    Update

--- src/Sample/src/org/csystem/util/wrapper/IntValue.java
+++ src/Sample/src/org/csystem/util/wrapper/IntValue.java
@@ -4,7 +4,7 @@
 	LAST UPDATE	: 19.03.2023
 
 	Immutable IntValue class that wraps an int value by using cache
-	for values int [-128, 127] interval
+	for values in [-128, 127] interval
 
 	Copyleft (c) 1993 C and System Programmers Association
 	All Rights Free
@@ -15,8 +15,12 @@ public class IntValue {
     private static final int CACHE_MIN = -128;
     private static final int CACHE_MAX = 127;
     private static final IntValue [] CACHE = new IntValue[CACHE_MAX - CACHE_MIN + 1];
-
     private final int m_value;
+
+    public static final IntValue ZERO = of(0);
+    public static final IntValue ONE = of(1);
+    public static final IntValue TWO = of(2);
+    public static final IntValue TEN = of(10);
 
     private IntValue(int value)
     {
@@ -25,11 +29,104 @@ public class IntValue {
 
     public static IntValue of(int value)
     {
-        //...
+        if (value < CACHE_MIN || CACHE_MAX < value)
+            return new IntValue(value);
+
+        int idx = value + 128;
+
+        if (CACHE[idx] == null)
+            CACHE[idx] = new IntValue(value);
+
+        return CACHE[idx];
     }
 
     public int getValue()
     {
         return m_value;
+    }
+
+    public int compareTo(IntValue other)
+    {
+        return m_value - other.m_value;
+    }
+
+    public IntValue add(int value)
+    {
+        return of(m_value + value);
+    }
+
+    public IntValue add(IntValue value)
+    {
+        return add(value.m_value);
+    }
+
+    public IntValue subtract(int value)
+    {
+        return add(-value);
+    }
+
+    public IntValue subtract(IntValue value)
+    {
+        return subtract(value.m_value);
+    }
+
+    public IntValue multiply(int value)
+    {
+        return of(m_value * value);
+    }
+
+    public IntValue multiply(IntValue value)
+    {
+        return multiply(value.m_value);
+    }
+
+    public IntValue divide(int value)
+    {
+        return of(m_value / value);
+    }
+
+    public IntValue divide(IntValue value)
+    {
+        return divide(value.m_value);
+    }
+
+    public IntValue [] divideAndRemainder(int value)
+    {
+        IntValue [] result = new IntValue[2];
+
+        result[0] = divide(value);
+        result[1] = mod(value);
+
+        return result;
+    }
+
+    public IntValue [] divideAndRemainder(IntValue value)
+    {
+        return divideAndRemainder(value.m_value);
+    }
+
+    public IntValue mod(int value)
+    {
+        return of(m_value % value);
+    }
+
+    public IntValue mod(IntValue value)
+    {
+        return mod(value.m_value);
+    }
+
+    public IntValue inc()
+    {
+        return add(1);
+    }
+
+    public IntValue dec()
+    {
+        return subtract(1);
+    }
+
+    public String toString()
+    {
+        return m_value + "";
     }
 }
