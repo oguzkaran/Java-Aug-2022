9562171229cf1b2c21a3115339b69ae32c9bf369
Oğuz Karan <oguzkaran@csystem.org>
2023-03-25 12:35:54 +0300

    Update

--- src/Sample/src/org/csystem/app/App.java
+++ src/Sample/src/org/csystem/app/App.java
@@ -1,20 +1,46 @@
 /*----------------------------------------------------------------------------------------------------------------------
-
------------------------------------------------------------------------------------------------------------------------*/
+	enum class'lar enum anahtar sözcüğü ile bildirilir. enum sınıfı içerisinde ilk noktalı virgülden önce (noktalı virgülün
+	zorunlu olduğıu durumlar vardır. Zorunlu değilse koymayacağız) bildirilen isimlere "enum sabitleri (enum constants)"
+	denir. Bu isimler public, static ve finak olarak bildirilmiş o enum sınıfı türünden referanslardır. Bu referansların
+	herbiri ilgili enum sınıfı türünden yaratılmış olan bir nesnenin adresini tutar. enum sabitleri için public, static,
+	final ve tür bilgisi yazılması geçersizdir
+----------------------------------------------------------------------------------------------------------------------*/
 package org.csystem.app;
-
-import org.csystem.util.math.geometry.Point;
 
 class App {
 	public static void main(String [] args)
 	{
-		Point p1 = Point.createCartesian(100, 100);
-		Point p2 = Point.createPolar(100, 50);
-
-		System.out.println(p1.toString());
-		System.out.println(p2.toString());
+		BirthDateApp.run();
 	}
 }
 
+class BirthDateApp {
+	public static void run()
+	{
+		//...
+		Date date = new Date();
+
+		date.setMonth(Month.SEP);
+		//...
+	}
+}
+
+enum Month {
+	JAN, FEB, MAR, APR, MAY, JUN, JUL, AUG, SEP, OCT, NOV, DEC
+}
+
+
+class Date {
+	private int m_month;
+
+	//...
+
+	public void setMonth(Month month)
+	{
+		//...
+	}
+
+	//...
+}
 
 
---
+++ src/Sample/src/org/csystem/util/math/Fraction.java
@@ -0,0 +1,161 @@
+/*----------------------------------------------------------------------------------------------------------------------
+	Sınıf Çalışması: Bir kesri temsil eden Fraction isimli sınıfı aşağıdaki açıklamalara göre yazınız
+	Açıklamalar:
+	+ Sınıf Matematikteki bir kesri temsil ettiğinden pay ve payda değerleri tutulacaktır
+
+	+ Sınıfın ilgili set ve get metotları yazılacaktır
+
+	+ Pay'ın sıfırdan farklı, paydanın sıfır olması durumunda tanımsızlığa ilişkin bir mesaj verilecektir, pay ve paydanın
+	her ikisinin birden sıfır olması durumunda belirsizliğe ilişkin mesaj verilecektir. Her iki durumda da program
+	sonlandırılacaktır
+
+	+ Kesir her durumda sadeleşmiş bir biçimde tutulacaktır. Örneğin kesrin pay ve paydası sırasıyla 4 ve 18 olarak
+	verildiğinde kesir 2 / 9 olarak tutulacaktır.
+
+	+ Kesir negatif ise işaret payda bulunacaktır. Örneğin kesrin pay ve paydası sırasıyla 3 ve -4 olarak verilmişse
+	kesir -3 / 4 biçiminde tutulacaktır
+
+	+ Kesrin pay ve paydasının ikisinin birden negatif olması durumunda kesir pozitif olarak tutulacaktır
+
+	+ Kesrin payının sıfır olması durumunda payda ne olursa olsun 1(bir) yapılacaktır
+
+	+ Sınıfın iki kesri toplayan, bir kesir ile bir tamsayıyı toplayan metotları olacaktır. Aynı işlemler
+	 çıkarma, çarpma ve bölme için de yapılacaktır
+
+	+ Sınıfın kesri 1(bir) artıran ve bir azaltan inc ve dec metotları yazılacaktır
+
+	+ Sınıfın toString metodu şu formatta yazı döndürecektir:
+	    3 / 10 kesri için -> 3 / 10 = 3.333333
+	    10 / 1 kesri için -> 10
+
+	 + Sınıfın default ctor'u "0 / 1" kesrini temsil eden nesneyi yaratmak için kullanılabilecektir
+
+	 + Sınıfın compareTo metodu iki kesrin büyüklük küçüklük karşılaştırmasını yapacaktır
+
+	 + Kesrin double türden ondalık değerini döndüren getRealValue metodu yazılacaktır
+
+	 + Sınıfın public bölümünü değiştirmeden istediğiniz değişikliği ve eklemeleri yapabilirsiniz
+
+	 Sınıfın iskelet bildirimi şu şekildedir:
+		package org.csystem.util.math;
+
+		public class Fraction {
+			private int m_a;
+			private int m_b;
+
+			public Fraction()
+			{
+				//TODO:
+			}
+
+			public Fraction(int a, int b)
+			{
+				//TODO:
+			}
+
+			public Fraction(int a)
+			{
+				//TODO:
+			}
+
+			public int getNumerator()
+			{
+				return m_a;
+			}
+
+			public void setNumerator(int val)
+			{
+				//TODO:
+			}
+
+			public int getDenominator()
+			{
+				return m_b;
+			}
+
+			public void setDenominator(int val)
+			{
+				//TODO:
+			}
+
+			public double getRealValue()
+			{
+				//TODO:
+			}
+
+			public Fraction add(Fraction other)
+			{
+				//TODO:
+			}
+
+			public Fraction add(int val)
+			{
+				//TODO:
+			}
+
+			public Fraction subtract(Fraction other)
+			{
+				//TODO:
+			}
+
+			public Fraction subtract(int val)
+			{
+				//TODO:
+			}
+
+			public Fraction multiply(Fraction other)
+			{
+				//TODO:
+			}
+
+			public Fraction multiply(int val)
+			{
+				//TODO:
+			}
+
+			public Fraction divide(Fraction other)
+			{
+				//TODO:
+			}
+
+			public Fraction divide(int val)
+			{
+				//TODO:
+			}
+
+			public void inc()
+			{
+				//TODO:
+			}
+
+			public void dec()
+			{
+				//TODO:
+			}
+
+			public int compareTo(Fraction other)
+			{
+				//TODO:
+			}
+
+			public String toString()
+			{
+				//TODO:
+			}
+		}
+----------------------------------------------------------------------------------------------------------------------*/
+/*----------------------------------------------------------------
+	FILE		: Fraction.java
+	AUTHOR		: Java-Aug-2022 Group
+	LAST UPDATE	: 25.03.2023
+
+	Fraction class that represents fraction
+
+	Copyleft (c) 1993 C and System Programmers Association
+	All Rights Free
+----------------------------------------------------------------*/
+package org.csystem.util.math;
+
+public class Fraction {
+    //...
+}
