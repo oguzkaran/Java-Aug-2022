8e96bf47883b1ddfe9199fc288859856daee3642
Oğuz Karan <oguzkaran@csystem.org>
2023-04-02 12:15:31 +0300

    Update

--- src/Sample/src/org/csystem/app/App.java
+++ src/Sample/src/org/csystem/app/App.java
@@ -1,5 +1,5 @@
 /*----------------------------------------------------------------------------------------------------------------------
-
+	super ctor sentaksı adından da anlaşılacağı gibi yalnızca ctor içerisinde kullanılabilir
 ----------------------------------------------------------------------------------------------------------------------*/
 package org.csystem.app;
 
@@ -10,11 +10,38 @@ class App {
 	}
 }
 
-
 class B extends A {
 	//...
+	public int b;
+
+	public B()
+	{
+		System.out.println("I am default ctor of B");
+	}
+
+	public B(int x)
+	{
+		super(x);
+		System.out.println("I am ctor of B with parameter type int");
+	}
+
+	public void foo(int x)
+	{
+		super(x); //error
+	}
 }
 
 class A {
 	//...
+	public int a;
+
+	public A()
+	{
+		System.out.println("I am default ctor of A");
+	}
+
+	public A(int x)
+	{
+		System.out.println("I am ctor of A with parameter type int");
+	}
 }
