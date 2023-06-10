3fa79de78fc9f0c45d2f9485f208c8c6d7433318
Ekrem Şallı <111192078+ekremsalli@users.noreply.github.com>
2023-06-10 15:14:10 +0300

    update

--- src/src-console/csd/App.java
+++ src/src-console/csd/App.java
@@ -2,16 +2,50 @@
 	
 -----------------------------------------------------------------------------------------------------------------------*/
 package csd;
-
+import java.io.File;
 class App {
 	public static void main(String [] args) 
 	{
 		System.out.println("App.main");		
 		msd.Sample.foo();
 		msd.Sample.main(args);
+	
+	        String projeDizini = "C:\\Users\\ekrem\\OneDrive\\Masaüstü\\AirlineTicketSystem\\AirlineTicket"; // Projeyi temsil eden ana dizini buraya girin
+	        File dizin = new File(projeDizini);
+	        int dosyaSayisi = JavaDosyaSayaci.dosyaSayisiniBul(dizin);
+	        System.out.println("Proje içindeki Java dosya sayısı: " + dosyaSayisi);
+	    
 	}	
 } 
 
-public class A {
+
+class JavaDosyaSayaci {
+    public static int dosyaSayisiniBul(File dizin) {
+        int dosyaSayisi = 0;
+
+        if (dizin.isDirectory()) {
+            File[] dosyalar = dizin.listFiles();
+
+            if (dosyalar != null) {
+                for (File dosya : dosyalar) {
+                    if (dosya.isFile() && dosya.getName().endsWith(".java")) {
+                        dosyaSayisi++;
+                    } else if (dosya.isDirectory()) {
+                        dosyaSayisi += dosyaSayisiniBul(dosya);
+                    }
+                }
+            }
+        }
+
+        return dosyaSayisi;
+    }
+
 
 }
+
+
+
+
+
+
+
---
+++ src/src-console/msd/Deneme.java
@@ -0,0 +1,5 @@
+package msd;
+
+public class Deneme {
+
+}
