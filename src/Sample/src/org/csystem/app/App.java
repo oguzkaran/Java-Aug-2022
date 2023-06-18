ab22fda70c603a51095583bbcec064a62137386f
Oğuz Karan <oguzkaran@csystem.org>
2023-06-18 17:05:32 +0300

    Update

--- src/Sample/src/org/csystem/app/App.java
+++ src/Sample/src/org/csystem/app/App.java
@@ -1,15 +1,19 @@
 /*-----------------------------------------------------------------------------------------------------------------------
-	Java 17 ile birlikte rasgele sayı üretimine ilişkin Randomgenerator isimli bir arayüz eklenmiştir. Random sınıfı da
-	bu arayüzü implemente etmiştir. Bu anlamda rasgele sayı üretiminie ilişkin nextXXX metotları gibi bir çok metot ile
-	rasgele üretim genelleştirilmiştir. RandomGenerator arayüzü ile aslında rasgele sayı üretimi algoritmik olarak
-	genellenmiştir
+	Generic açılımlarda temel türler için wrapper sınıf karşılıkları kullanılır
 -----------------------------------------------------------------------------------------------------------------------*/
 package org.csystem.app;
+
+import java.util.ArrayList;
 
 class App {
 	public static void main(String [] args)
 	{
 
 	}
+}
+
+class A<T> {
+	private T m_t;
+	//...
 }
 
