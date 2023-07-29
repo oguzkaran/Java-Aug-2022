a42b6ac9d07ce29634aec764ef9b0ce52bde4d27
Oğuz Karan <oguzkaran@csystem.org>
2023-07-29 15:08:55 +0300

    Update

--- src/Sample/src/org/csystem/app/Application.java
+++ src/Sample/src/org/csystem/app/Application.java
@@ -10,24 +10,7 @@ import static org.csystem.util.console.commandline.CommandLineArgsUtil.checkLeng
 public class Application {
     public static void run(String[] args)
     {
-        checkLengthEquals(args.length, 2, "Wrong number of arguments!...");
-
-        try {
-            Files.move(Path.of(args[0]), Path.of(args[1]));
-            Console.writeLine("File moved successfully...");
-        }
-        catch (DirectoryNotEmptyException ignore) {
-            Console.writeErrLine("'%s' directory not empty", args[1]);
-        }
-        catch (NoSuchFileException ignore) {
-            Console.writeErrLine("'%s' not exists", args[0]);
-        }
-        catch (IOException ignore) {
-            Console.writeErrLine("I/O problem occurs!...");
-        }
-        catch (InvalidPathException ex) {
-            Console.writeErrLine("'%s' is invalid path...",  ex.getInput());
-        }
+        Console.writeLine(System.getProperty("line.separator").equals("\r\n"));
     }
 
     public static void main(String[] args)
